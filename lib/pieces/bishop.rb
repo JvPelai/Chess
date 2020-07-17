24c728313173d9b77c16513a33c7eac989702d05
jvpelai <jv.pelai@gmail.com>
2020-07-17 14:58:38 -0300

    adjusted movement constraints

--- lib/pieces/bishop.rb
+++ lib/pieces/bishop.rb
@@ -1,11 +1,9 @@
-require 'pry'
 module Moves
     def white_bishop_moves(start,destination)
         startpiece = @gameboard[start[0]][start[1]-1]
-        p startpiece
         endpiece= @gameboard[destination[0]][destination[1]-1]
         moves = [[1,1],[1,-1],[-1,-1],[-1,1]]
-        move = [destination[0]-start[0],start[1]-destination[1]]
+        move = [destination[0]-start[0],destination[1]-start[1]] 
         if move[0] == 0 || move[1] == 0
             puts"Invalid move!"
             return
@@ -16,14 +14,17 @@ module Moves
             end
         end
         move[0],move[1] = move[0]/move[0].abs, move[1]/move[1].abs
-        p move 
         if move[0] == move[1]  && move[0] < 0 #upleft
+            return if !upleft_valid?(start,destination,"white")
             white_piece_move(start,destination,startpiece,endpiece,moves,move)
         elsif move[0] < move[1] && move[0] < 0 #upright
+            return if !upright_valid?(start,destination,"white")
             white_piece_move(start,destination,startpiece,endpiece,moves,move)
         elsif move[0] == move[1] && move[0] > 0 #downright
+            return if !downright_valid?(start,destination,"white")
             white_piece_move(start,destination,startpiece,endpiece,moves,move)
         elsif move[0] > move[1] && move[0] > 0 #downleft
+            return if !downleft_valid?(start,destination,"white")
             white_piece_move(start,destination,startpiece,endpiece,moves,move)
         end
     end
@@ -32,7 +33,7 @@ module Moves
         startpiece = @gameboard[start[0]][start[1]-1]
         endpiece= @gameboard[destination[0]][destination[1]-1]
         moves = [[1,1],[1,-1],[-1,-1],[-1,1]]
-        move = [destination[0]-start[0],start[1]-destination[1]]
+        move = [destination[0]-start[0],destination[1]-start[1]]
         if move[0] == 0 || move[1] == 0
             puts"Invalid move!"
             return
@@ -44,22 +45,73 @@ module Moves
         end
         move[0],move[1] = move[0]/move[0].abs, move[1]/move[1].abs
         if move[0] == move[1] && move[0] > 0 #downright
+            return if !downright_valid?(start,destination,"black")
             black_piece_move(start,destination,startpiece,endpiece,moves,move)
         elsif move[0] > move[1] && move[0] > 0 #downleft
+            return if !downleft_valid?(start,destination,"black")
             black_piece_move(start,destination,startpiece,endpiece,moves,move)
         elsif move[0] == move[1]  && move[0] < 0 #upleft
+            return if !upleft_valid?(start,destination,"black")
             black_piece_move(start,destination,startpiece,endpiece,moves,move)
         elsif move[0] < move[1] && move[0] < 0 #upright
+            return if !upright_valid?(start,destination,"black")
             black_piece_move(start,destination,startpiece,endpiece,moves,move)
         end
     end
 
-    def upright_valid?(tracked,color)
-        pieces = get_color(color)
-        if pieces.include?(tracked)
-            false
-        else
-            true
+    def upright_valid?(start,destination,color)
+        pieces = get_pieces(color)
+        i = 1
+        while i < destination[1] - start[1]
+            tracked = @gameboard[start[0]-i][start[1]-1+i]
+            if pieces.include?(tracked)
+                puts "Invalid move"
+                return false
+            else
+                i += 1
+            end
+        end
+    end
+
+    def upleft_valid?(start,destination,color)
+        pieces = get_pieces(color)
+        i = 1
+        while i < start[0] - destination[0]
+            tracked = @gameboard[start[0]-i][start[1]-1-i]
+            if pieces.include?(tracked)
+                puts "Invalid move"
+                return false
+            else
+                i += 1
+            end
+        end
+    end
+
+    def downright_valid?(start,destination,color)
+        pieces = get_pieces(color)
+        i = 1
+        while i < destination[0] - start[0]
+            tracked = @gameboard[start[0]+i][start[1]-1+i]
+            if pieces.include?(tracked)
+                puts "Invalid move"
+                return false
+            else
+                i += 1
+            end
+        end
+    end
+
+    def downleft_valid?(start,destination,color)
+        pieces = get_pieces(color)
+        i = 1
+        while i < destination[0] - start[0]
+            tracked = @gameboard[start[0]+i][start[1]-1-i]
+            if pieces.include?(tracked)
+                puts "Invalid move"
+                return false
+            else
+                i += 1
+            end
         end
     end
         
